186e1494b11c007d4f5e4731c58079d4836d96d5
Guilherme Nunes <guilherme.augd@gmail.com>
2025-11-14 16:46:18 -0300

    feature: header refurbished

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { CartProvider } from "@/context/CartContext";
+import { NavbarDemo } from "./components/ui/navbar/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-700 text-white min-h-screen flex flex-col`}
       >
         <CartProvider>
-          <Header/>
+          <NavbarDemo />
           <main className="grow">
             {children}
           </main>
